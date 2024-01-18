a5b4e7cff9f16ec5362ba965117bbf8bc9928aa3
Maverick <savagebangbang09@gmail.com>
2024-01-18 13:28:41 +0700

    alhamdulillah

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'postingan/index'
   get '/index' => 'home#index'
+  get '/postingan' => 'postingan#index'
   get '/artikel' => 'home#artikel'
   get '/about' => 'home#about'
+  get '/' => 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
